870961c328816a0bf8bfdf5c156b98e0626fccdb
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-11-13 22:40:12 +0100

    Doors open & close

--- src/game/door.c
+++ src/game/door.c
@@ -2,16 +2,21 @@
 
 void	door(t_game *game)
 {
+	t_vector	door;
 	t_ray	*aim;
 
 	aim = &game->world.rays[game->mlx->width / 2];
-	if (aim->length > DOOR_DISTANCE * TILE_SIZE
-		|| is_door(game, aim->hit) == false)
+	if (get_distance_between(aim->d_hor, zero_vector())
+		< get_distance_between(aim->d_ver, zero_vector()))
+		door = vector_sum(aim->origin, aim->d_hor);
+	else
+		door = vector_sum(aim->origin, aim->d_ver);
+	if (is_door(game, door) == false)
 		return ;
-	game->flex_map.map[aim->hit.x / TILE_SIZE][aim->hit.y / TILE_SIZE] -= '0';
-	game->flex_map.map[aim->hit.x / TILE_SIZE][aim->hit.y / TILE_SIZE]++;
-	game->flex_map.map[aim->hit.x / TILE_SIZE][aim->hit.y / TILE_SIZE] %= 2;
-	game->flex_map.map[aim->hit.x / TILE_SIZE][aim->hit.y / TILE_SIZE] += '0';
+	game->flex_map.map[door.x / TILE_SIZE][door.y / TILE_SIZE] -= '0';
+	game->flex_map.map[door.x / TILE_SIZE][door.y / TILE_SIZE]++;
+	game->flex_map.map[door.x / TILE_SIZE][door.y / TILE_SIZE] %= 2;
+	game->flex_map.map[door.x / TILE_SIZE][door.y / TILE_SIZE] += '0';
 	draw_minimap(game);
 	render_world(game);
 }
