dde0793b232ff2cf175799f2c167179d76d759a2
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-11-13 17:55:51 +0100

    merge main src/main.c

--- src/main.c
+++ src/main.c
@@ -14,10 +14,13 @@ int	main(int argc, char **argv)
 	parse(&game, argc, argv);
 	setup_game(&game);
 	mlx_loop_hook(game.mlx, hook, &game);
-	mlx_mouse_hook(game.mlx, reset_zoom, &game);
+	mlx_key_hook(game.mlx, keyboard, &game);
 	mlx_scroll_hook(game.mlx, zoom_minimap, &game);
+	mlx_mouse_hook(game.mlx, mouse, &game);
+	mlx_resize_hook(game.mlx, resize_window, &game);
 	mlx_loop(game.mlx);
 	free_game(&game);
-	printf("\033[K\rThanks for playing\n");
+	if (PRINT)
+		printf("\033[K\rThanks for playing\n");
 	return (EXIT_SUCCESS);
 }
